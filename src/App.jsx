9bc0238461345887c8619db19133ee5eaf10781e
Jaime Alberto Suarez Moctezuma <154639506+JaimeMoc@users.noreply.github.com>
2024-01-11 17:10:14 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,9 @@ const App = () => {
       <Header />
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
+      <Education />
       <Portfolio />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
-      <Education />
     </div>
   );
 };
