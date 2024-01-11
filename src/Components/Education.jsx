bb24887197ce47a362f52c9deb8c93e222029dbc
Jaime Alberto Suarez Moctezuma <154639506+JaimeMoc@users.noreply.github.com>
2024-01-11 16:48:50 -0600

    Create Education.jsx

---
+++ src/Components/Education.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+          <P>Ingenieria en sistemas computacionales. Tecnologico de Tuxtla Gutierrez.</P>
+          <p>Embajador Estudiantil de Microsoft.</p>
+        </section>
+    )
+}
+
+export default Education;
