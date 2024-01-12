8cb6ffeb1528ad6a417473db5bbca8c94ebd9bec
Jaime Alberto Suarez Moctezuma <154639506+JaimeMoc@users.noreply.github.com>
2024-01-12 13:03:06 -0600

    Update Education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,7 +4,7 @@ const Education = () => {
     return(
         <section className="light" id="Education">
             <h2 style={{textAlign: "center"}}>Education</h2>
-            <ul>
+            <ul style={{textAlign: "center"}}>
             <li>Estudiante de la Ingeniería en sistemas computacionales. Tecnológico de Tuxtla Gutiérrez.</li>
             <li>Embajador Estudiantil de Microsoft.</li>
             </ul>
